668fa3324dcbd99a728efcf1b2e74b7f54f29da5
wuhaojie <w19961009@126.com>
2015-08-24 22:39:43 +0800

    getAllWifi()函数完成.

--- app/src/main/java/com/voyager/wifi/MainActivity.java
+++ app/src/main/java/com/voyager/wifi/MainActivity.java
@@ -1,37 +1,39 @@
 package com.voyager.wifi;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+import com.voyager.wifi.com.voyager.wifi.utils.Wifi;
+import com.voyager.wifi.com.voyager.wifi.utils.WifiUtils;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<Wifi> wifiList = new WifiUtils().getAllWifi();
+        System.out.println(wifiList);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_about) {
+            startActivity(new Intent(this, AboutActivity.class));
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
